60f4a69a77795774cc3eba402c974b4ca87c7b24
Bob Adewusi <60587384+Bobaddey@users.noreply.github.com>
2021-11-12 11:09:46 +0100

    Update nav.jsx

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './nav.css';
 
-
+const style ={
+    // backgroundColor: "rgb(24, 117, 145)",
+    padding: "20px 60px",
+    fontSize: "20px",
+    borderRadius: "10px",
+    fontFamily: 'Ubuntu',
+    color: "rgb(24, 117, 145)",
+   border: "none",
+   marginRight: "15px"
+}
 const Nav = () => {
     return (
         <nav className="menubar">
-            <ul className="menu">
-                <button className="menuItem">Public Profile</button>
-                <button className="menuItem">Account</button>
-                <button className="menuItem">Availability</button>
-                <button className="menuItem">Support</button>
+            <ul className="">
+                <li style={{}} style={style} className="menuItem">Public Profile</li>
+                <li style={style} className="menuItem">Account</li>
+                <li style={style} className="menuItem">Availability</li>
+                <li style={style} className="menuItem">Support </li>
             </ul>
       </nav>
     )
